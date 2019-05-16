753b9c3c724f4f2fd258c10038a61d9cc248d9ab
Anubhav311 <anubhavbhambri1@gmail.com>
2019-05-17 02:33:41 +0530

    created database

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 
+// const dishesRouter = require('../router/dishes-router');
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+// server.use('/api/dishes', dishesRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json('Hi')
